75b85b160a7b62db6013675d0195ae655d95aa4e
dejanstojanovic <dejan@adxs.com>
2017-03-05 17:49:27 +0400

    Added sample project for NORTHWIND database

---
+++ DataBinding.Sample/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding.Sample.Models
+{
+   public class Customer
+    {
+        [DataAccess.DataBind("CustomerId")]
+        public long ID { get; set; }
+
+        [DataAccess.DataBind("First_Name")]
+        public String FirstName { get; set; }
+
+        [DataAccess.DataBind("Last_Name")]
+        public String LastName { get; set; }
+    }
+}
