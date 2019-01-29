cc614bcf1e8930dfeca99467e707f0553b714a1f
Dejan Stojanovic <dejan@mobitrans.net>
2019-01-29 15:35:37 +0400

    Updates test app

---
+++ Databind.SampleApp/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using Databind.Binding.Attributes;
+using System;
+
+namespace Databind.SampleApp.Models
+{
+   public class Customer
+    {
+        [PropertyBind("CustomerID")]
+        public String ID { get; set; }
+
+        [PropertyBind("CompanyName")]
+        public String Company { get; set; }
+
+        [PropertyBind("ContactName")]
+        public String ContactPerson { get; set; }
+
+        [PropertyBind("Address")]
+        public String Address { get; set; }
+    }
+}
---
+++ Databind.SampleApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Diagnostics;
+using Databind.SampleApp.Models;
+using Databind.Providers;
+using Databind.Extensions;
+
+namespace Databind.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch timer = new Stopwatch();
+
+            using (var dal = new SqlProvider(ConfigurationManager.ConnectionStrings["db.connection"].ToString()))
+            {
+                var result = dal.ExecuteDataTable(
+                "Orders_GetAll");//.ToModels<Order>().ToList();
+
+            }
+
+            ////SqlDataReader reading to models
+            timer.Start();
+            using (var dal = new SqlProvider(ConfigurationManager.ConnectionStrings["db.connection"].ToString()))
+            {
+                var result = dal.ExecuteModels<Order>(
+                "Orders_GetAll").ToList();
+
+            }
+            timer.Stop();
+            Console.WriteLine($"ExecuteModels: {timer.ElapsedMilliseconds}");
+
+
+            //DataTable to model
+            timer.Restart();
+            using (var dal = new SqlProvider(ConfigurationManager.ConnectionStrings["db.connection"].ToString()))
+            {
+                var result = dal.ExecuteDataTable(
+                "Orders_GetAll").ToModels<Order>().ToList();
+
+            }
+            timer.Stop();
+            Console.WriteLine($"ExecuteDataTable.ToModels: {timer.ElapsedMilliseconds}" );
+
+            //DataTable only
+            timer.Restart();
+            using (var dal = new SqlProvider(ConfigurationManager.ConnectionStrings["db.connection"].ToString()))
+            {
+                var result = dal.ExecuteDataTable(
+                "Orders_GetAll"
+                );//.ToModels<Order>().ToList();
+
+            }
+            timer.Stop();
+            Console.WriteLine($"ExecuteDataTable: {timer.ElapsedMilliseconds}");
+
+
+            Console.ReadLine();
+        }
+    }
+}
