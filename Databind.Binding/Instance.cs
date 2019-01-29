93c3a3e789d9e3bd6c2e8fa06e1fedd60577e923
Dejan Stojanovic <dejan@mobitrans.net>
2019-01-29 10:39:58 +0400

    Adds performance tests for accessing properties

--- Databind.Binding/Instance.cs
+++ Databind.Binding/Instance.cs
@@ -9,7 +9,9 @@ namespace Databind.Binding
     public static class Instance<T> where T : new()
     {
         public static readonly Func<T> New;
-        private static Dictionary<String, (PropertyInfo Info, Func<T, Object> Get, Action<T,Object> Set)> properties;
+        private static Dictionary<String, (PropertyInfo Info, Func<T, Object> Get, Action<T, Object> Set)> properties;
+
+        public static IEnumerable<String> GetPropertyNames() => properties.Keys;
 
         static Instance()
         {
@@ -23,64 +25,30 @@ namespace Databind.Binding
                 properties.Add(
                    propertyInfo.Name, (
                    Info: propertyInfo,
+                   Get: Expression.Lambda<Func<T, Object>>(
+                          Expression.Convert(
+                              Expression.Call(instanceParam, propertyInfo.GetGetMethod()),
+                              typeof(Object)
+                          ),
+                        instanceParam).Compile(),
 
-                    //Get: Expression.Lambda<Func<T, Object>>(
-                    // Expression.Convert(
-                    //     Expression.Property(instanceParam, propertyInfo.Name),
-                    //     typeof(Object)
-                    // ),
-                    // instanceParam).Compile(),
-
-                    Get: Expression.Lambda<Func<T, Object>>(
-                     Expression.Convert(
-                         Expression.Call(instanceParam, propertyInfo.GetGetMethod()),
-                         typeof(Object)
-                     ),
-
-                     instanceParam).Compile(),
-
-                    Set: Expression.Lambda<Action<T, Object>>(
-                        Expression.Call(instanceParam, propertyInfo.GetSetMethod(), Expression.Convert(argumentParam, propertyInfo.PropertyType)),
-                        instanceParam,argumentParam
-                    ).Compile()
+                   Set: Expression.Lambda<Action<T, Object>>(
+                          Expression.Call(instanceParam, propertyInfo.GetSetMethod(), Expression.Convert(argumentParam, propertyInfo.PropertyType)),
+                          instanceParam, argumentParam
+                        ).Compile()
 
                     ));
-
-                //TODO: Get attribute
-
-                //var typeParam = Expression.Parameter(typeof(Type));
-                //var attribute = Expression.Lambda<Func<T, Type, Object>>(
-                //    Expression.Convert(
-                //        Expression.Call(instanceParam,)
-                //            , typeof(Object)
-                //        )
-                //    ).Compile();
-
             }
         }
 
         public static Object Get(T instance, String propertyName)
         {
-            if(properties.TryGetValue(propertyName, out var property))
-            {
-                return property.Get(instance);
-            }
-            else
-            {
-                throw new PropertyNotFoundException(instance, propertyName);
-            }
+            return properties[propertyName].Get(instance);
         }
 
         public static void Set(T instance, String propertyName, Object value)
         {
-            if (properties.TryGetValue(propertyName, out var property))
-            {
-                property.Set(instance, value);
-            }
-            else
-            {
-                throw new PropertyNotFoundException(instance, propertyName);
-            }
+            properties[propertyName].Set(instance, value);
         }
 
     }
--- Databind.Tests/InstanceTests.cs
+++ Databind.Tests/InstanceTests.cs
@@ -1,12 +1,18 @@
 using Databind.Binding;
 using Databind.Tests.Models;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Xunit;
+using System.Linq;
+using System.IO;
 
 namespace Databind.Tests
 {
     public class InstanceTests
     {
+        int dataCollectionSize = 1000000;
+
         [Fact]
         public void TestPropertyGet()
         {
@@ -25,6 +31,73 @@ namespace Databind.Tests
 
             Assert.IsAssignableFrom<int>(number);
             Assert.True((int)number == 12);
+
+        }
+
+        [Fact]
+        public void TestSpeedNormal()
+        {
+            var data = new List<SampleGenericModel>();
+            for (int i = 0; i < dataCollectionSize; i++)
+            {
+                data.Add(new SampleGenericModel(Guid.NewGuid(), DateTime.Now));
+            }
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Reset();
+            stopWatch.Start();
+            var vals1 = data.Select(d => new {
+                Id = d.Id,
+                Time = d.Time
+            }).ToList();
+            stopWatch.Stop();
+            Debug.WriteLine($"N: {stopWatch.ElapsedMilliseconds}");
+
+        }
+
+        [Fact]
+        public void TestSpeedLambda()
+        {
+            var data = new List<SampleGenericModel>();
+            for (int i = 0; i < dataCollectionSize; i++)
+            {
+                data.Add(new SampleGenericModel(Guid.NewGuid(), DateTime.Now));
+            }
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Reset();
+            stopWatch.Start();
+            var vals2 = data.Select(d => new
+            {
+                Id = Instance<SampleGenericModel>.Get(d, "Id"),
+                Time = Instance<SampleGenericModel>.Get(d, "Time")
+            }).ToList();
+            stopWatch.Stop();
+            Debug.WriteLine($"L: {stopWatch.ElapsedMilliseconds}");
+        }
+
+        [Fact]
+        public void TestSpeedReflection()
+        {
+            var data = new List<SampleGenericModel>();
+            for (int i = 0; i < dataCollectionSize; i++)
+            {
+                data.Add(new SampleGenericModel(Guid.NewGuid(), DateTime.Now));
+            }
+
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Reset();
+            stopWatch.Start();
+            var vals1 = data.Select(d => new
+            {
+                Id = d.GetType().GetProperty("Id").GetValue(d),
+                Time = d.GetType().GetProperty("Time").GetValue(d)
+            }).ToList();
+            stopWatch.Stop();
+            Debug.WriteLine($"R: {stopWatch.ElapsedMilliseconds}");
 
         }
     }
